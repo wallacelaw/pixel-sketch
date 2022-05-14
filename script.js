ae99f7e0f707b3bb99857575efa0fc4fa357e0fd
Wallace Law <wallace.law@hotmail.com>
2022-05-14 00:24:23 -0400

    Add 16x16 grid

---
+++ script.js
@@ -0,0 +1,21 @@
+// const content = document.createElement('div');
+// content.classList.add('content');
+// content.textContent = 'This is the glorious text-content!';
+
+
+// Create 16x16 grid of square divs
+
+
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        document.getElementById("container").appendChild(grid);
+    }
+    let jump = document.createElement("br");
+    document.getElementById("container").appendChild(jump);
+}
+
+
+
