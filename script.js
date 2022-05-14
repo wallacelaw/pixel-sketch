ca6c6a8082d34afa8f6c46250835dc14bc3cc7c1
Wallace Law <wallace.law@hotmail.com>
2022-05-14 02:25:52 -0400

    Add black paint tool

--- script.js
+++ script.js
@@ -4,18 +4,18 @@
 
 
 // Create 16x16 grid of square divs
-
-
-
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
+        grid.addEventListener("click", changeColor);
         document.getElementById("container").appendChild(grid);
     }
     let jump = document.createElement("br");
     document.getElementById("container").appendChild(jump);
 }
 
-
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
 
