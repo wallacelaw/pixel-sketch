42b8984834fb814918797109460b4f91c1e23e5b
Wallace Law <wallace.law@hotmail.com>
2022-05-14 14:10:23 -0400

    Add reset function

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
+//initialise buttons
+const eraserBtn = document.getElementById('eraser');
+const resetBtn = document.getElementById('reset');
+let grid = document.createElement('div');
+
+resetBtn.onclick = () => resetGrid();
 
 // set initial mousedown state to allow hold and click drawing
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
+
 // Create 16x16 grid of square divs
-for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 20; j++) {
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        grid.addEventListener("mouseover", changeColor);
-        grid.addEventListener("mousedown", () => mouseDown = true);
-        document.getElementById("container").appendChild(grid);
+function createGrid() {
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 20; j++) {
+            grid = document.createElement('div');
+            grid.classList.add('grid');
+            grid.addEventListener("mouseover", changeColor);
+            grid.addEventListener("mousedown", () => mouseDown = true);
+            document.getElementById("container").appendChild(grid);
+        }
+        let nextLine = document.createElement("br");
+        document.getElementById("container").appendChild(nextLine);
     }
-    let jump = document.createElement("br");
-    document.getElementById("container").appendChild(jump);
 }
 
+
+// paint grids
 function changeColor(e) {
     console.log(e.type);
     if (e.type === 'mouseover' && !mouseDown) return
     e.target.style.backgroundColor = 'black';
 }
+
+
+function resetGrid() {
+    let gridCells = Array.from(document.getElementsByClassName('grid'));
+    gridCells.forEach(grid => {
+        grid.style.backgroundColor = 'white';
+    });
+}
+
+//On page load
+createGrid();
 
